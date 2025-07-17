4527ba868d27ba1fcf0bb7c73089f1b28f909bf0
Dylan Rochon-Terry <dylanjrt@gmail.com>
2025-07-17 11:29:39 -0400

    Make footer smaller

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="bg-background mt-auto flex w-full items-center justify-end pr-24 pb-8 text-lg">
+    <footer className="bg-background text-secondary mt-auto flex w-full items-center justify-end pr-24 pb-8 text-sm">
       <span>
         site by:{" "}
         <a
